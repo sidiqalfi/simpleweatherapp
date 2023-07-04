2f65bc263c4287f8858ea0030c49cf2980b556a0
sidiqalfi <sidiqtamvan@gmail.com>
2023-07-04 17:03:10 +0700

    refactor : add some functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,14 +4,8 @@ const apiKey = "Your API Key";
 const submit = document.getElementById("submit");
 submit.addEventListener("click", async () => {
   try {
-    // css for main page
-    let box = document.querySelector(".box");
-    let main = document.querySelector(".main");
-    box.style.padding = "2rem";
-    main.style.display = "block";
-
     // get data from input
-    let city = document.getElementById("city").value;
+    const city = document.getElementById("city").value;
     const data = await fetchData(city);
 
     // showing data to result page
@@ -38,32 +32,16 @@ async function fetchData(city) {
 // Show data to result page
 function showData(data) {
   if (data.cod == 200) {
-    let temperatureData = document.querySelector(".main-temperature-data");
-    let temperatureDescription = document.querySelector(
-      ".main-temperature-description"
-    );
-    let humidity = document.querySelector(".second-layout-data-percentage");
-    let windSpeed = document.querySelector(".second-layout-data-speed");
+    mainPage(data);
 
-    temperatureData.innerHTML = Math.floor(data.main.temp);
-    temperatureDescription.innerHTML = data.weather[0].description;
-    humidity.innerHTML = data.main.humidity + "%";
-    windSpeed.innerHTML = data.wind.speed + "Km/h";
+    // change the weather image
+    changeImage(data.weather[0].main);
 
     // hidden error page
     let error = document.querySelector(".error");
     error.style.display = "none";
-
-    // change the weather image
-    changeImage(data.weather[0].main);
   } else {
-    // display the error page
-    let error = document.querySelector(".error");
-    error.style.display = "block";
-
-    // hidden the result page
-    let main = document.querySelector(".main");
-    main.style.display = "none";
+    errorPage();
   }
 }
 
@@ -79,4 +57,34 @@ function changeImage(data) {
 
   let image = document.querySelector(".weather-img");
   image.setAttribute("src", imagePaths[data] || "");
+}
+
+function errorPage() {
+  // display the error page
+  let error = document.querySelector(".error");
+  error.style.display = "block";
+
+  // hidden the result page
+  let main = document.querySelector(".main");
+  main.style.display = "none";
+}
+
+function mainPage(data) {
+  // css for main page
+  let box = document.querySelector(".box");
+  let main = document.querySelector(".main");
+  box.style.padding = "2rem";
+  main.style.display = "block";
+
+  let temperatureData = document.querySelector(".main-temperature-data");
+  let temperatureDescription = document.querySelector(
+    ".main-temperature-description"
+  );
+  let humidity = document.querySelector(".second-layout-data-percentage");
+  let windSpeed = document.querySelector(".second-layout-data-speed");
+
+  temperatureData.innerHTML = Math.floor(data.main.temp);
+  temperatureDescription.innerHTML = data.weather[0].description;
+  humidity.innerHTML = data.main.humidity + "%";
+  windSpeed.innerHTML = data.wind.speed + "Km/h";
 }
